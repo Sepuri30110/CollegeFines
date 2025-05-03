0fe44662435588986c9413780e4c6bdbec3dd411
Sepuri30110 <furious30110@gmail.com>
2025-05-03 21:43:32 +0530

    Frontend Paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import Student from './pages/Student';
 
 function App() {
   return (
-    <div>App</div>
+    <div style={{width:'100vw', height:'100vh'}}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/Student' element={<Student />} />
+          <Route exact path='/Admin' element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
